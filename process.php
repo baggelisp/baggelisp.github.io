f622b4460080e18e582e0bc9bcbcf9ca1719fca3
baggelisp <baggelisp.kef@hotmail.com>
2018-10-12 23:20:43 +0300

    form

---
+++ process.php
@@ -0,0 +1,24 @@
+<?php ob_start();
+
+	if(isset($_POST['submit'])){
+
+		$to="baggelisp.kef@hotmail.com";
+		$subject=$_POST['subject'];
+		$email=$_POST['email'];
+		$txt=$_POST['message'];
+		$headers="From: " {$email}  . "\r\n" .
+		"CC: somebodyelse@example.com";
+
+		mail($to,$subject,$txt,$headers);
+
+		header("Location: contact.html");
+
+
+
+
+	}
+
+
+
+
+?>
